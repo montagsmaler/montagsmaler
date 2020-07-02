c85d4bcab680485bb81af31fae70c6659114ea4a
Jannik Smidt <js343@hdm-stuttgart.de>
2020-07-02 12:40:16 +0200

    test not implemented but no errors

--- montagsmaler-api/src/api/http/s3/service/s3.service.spec.ts
+++ montagsmaler-api/src/api/http/s3/service/s3.service.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { S3Service } from './s3.service';
+import { S3Provider } from './s3.provider';
 
 describe('S3Service', () => {
   let service: S3Service;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [S3Service],
-    }).compile();
+      providers: [...S3Provider,S3Service],
+    })
+    .overrideProvider('S3_provider')
+    .useValue({
+      accessKeyId: '',
+      secretAccessKey: '',
+      sessionToken: '',
+    })
+    .compile();
 
     service = module.get<S3Service>(S3Service);
   });
