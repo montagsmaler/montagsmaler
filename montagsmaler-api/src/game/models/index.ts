4ba4fbefa449e10f540ec412ca8d94de50ca4d7a
Niklas Schildhauer <ns107@hdm-stuttgart.de>
2020-07-03 17:42:10 +0200

    new models: game and image

--- montagsmaler-api/src/game/models/index.ts
+++ montagsmaler-api/src/game/models/index.ts
@@ -1,4 +1,6 @@
 export * from './lobby';
-export * from  './player';
+export * from './player';
 export * from './lobby.event';
 export * from './lobby.playerjoined.event';
+export * from './game';
+export * from './image';
--- montagsmaler-api/src/game/models/player.ts
+++ montagsmaler-api/src/game/models/player.ts
@@ -1,8 +1,37 @@
+import { Image } from './image';
 import { JSONSerializable } from '../../shared/serializable';
 
 @JSONSerializable()
 export class Player {
-	private score = 0;
+  private score = 0;
+  private drawnImages: [Image];
 
-	constructor(public readonly id: string, public readonly name: string) { }
+  constructor(public readonly id: string, public readonly name: string) {}
+
+  public scorePoints(points: number): void {
+    this.score = this.score + points;
+  }
+
+  public getScore(): number {
+    return this.score;
+  }
+
+  public addImage(image: Image): void {
+    this.drawnImages.push(image);
+  }
+
+  public getImageInRound(round: number) {
+    const image = this.drawnImages.filter(function(item) {
+      return item.getRound() === round;
+    });
+    if (image[0] === undefined)
+      throw Error(
+        `Player "${this.name}" did not draw an image in round "${round}".`,
+      );
+    return image[0];
+  }
+
+  public getImages(): [Image] {
+    return this.drawnImages;
+  }
 }
