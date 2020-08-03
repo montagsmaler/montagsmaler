bcaf79a5234697aa51afed0e94c47cb6ca137489
Niklas Schildhauer <ns107@hdm-stuttgart.de>
2020-08-03 21:15:05 +0200

    Countdown with slide in out animation

--- montagsmaler-frontend/src/app/components/game/countdown/countdown.component.ts
+++ montagsmaler-frontend/src/app/components/game/countdown/countdown.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-countdown',
-  templateUrl: './countdown.component.html',
-  styleUrls: ['./countdown.component.scss']
+  selector: "app-countdown",
+  templateUrl: "./countdown.component.html",
+  styleUrls: ["./countdown.component.scss"],
+  animations: [
+    trigger("enterAnimation", [
+      transition(":enter", [
+        style({ transform: "translateY(-100%)" }),
+        animate("500ms ease-in", style({ transform: "translateY(0)" })),
+      ]),
+      transition(":leave", [
+        style({ transform: "translateY(0)", opacity: 1 }),
+        animate("500ms", style({ transform: "translateY(-100%)" })),
+      ]),
+    ]),
+  ],
 })
 export class CountdownComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- montagsmaler-frontend/src/app/components/game/game.component.ts
+++ montagsmaler-frontend/src/app/components/game/game.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {
   trigger,
+  query,
+  animateChild,
   state,
   style,
   animate,
@@ -12,15 +14,8 @@ import {
   templateUrl: "./game.component.html",
   styleUrls: ["./game.component.scss"],
   animations: [
-    trigger("enterAnimation", [
-      transition(":enter", [
-        style({ transform: "translateX(100%)", opacity: 0 }),
-        animate("500ms", style({ transform: "translateX(0)", opacity: 1 })),
-      ]),
-      transition(":leave", [
-        style({ transform: "translateX(0)", opacity: 1 }),
-        animate("500ms", style({ transform: "translateX(100%)", opacity: 0 })),
-      ]),
+    trigger("ngIfAnimation", [
+      transition(":enter, :leave", [query("@*", animateChild())]),
     ]),
   ],
 })
