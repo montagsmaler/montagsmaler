b8b36b817d130217f4168d95569e4f0ae9bd4a31
Niklas Schildhauer <ns107@hdm-stuttgart.de>
2020-08-04 14:21:01 +0200

    copy link

--- montagsmaler-frontend/src/app/components/game-lobby/lobby-item/lobby-item.component.ts
+++ montagsmaler-frontend/src/app/components/game-lobby/lobby-item/lobby-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { getRandomColor } from '../../../utility/utility.js';
 
 @Component({
   selector: 'app-lobby-item',
@@ -33,22 +34,6 @@ export class LobbyItemComponent implements OnInit {
   }
 
   getRandomColor(inputNumber: number) {
-    const random = inputNumber % 7;
-    switch (random) {
-      case 0:
-        return '#00D5B9';
-      case 1:
-        return '#E2B236';
-      case 2:
-        return '#E1325B';
-      case 3:
-        return '#028DCB';
-      case 4:
-        return '#F47E4A';
-      case 5:
-        return '#4A86F4';
-      case 6:
-        return '#F48B4A';
-    }
+    return getRandomColor(inputNumber);
   }
 }
--- montagsmaler-frontend/src/app/components/game/wait-for-players/player-item/player-item.component.ts
+++ montagsmaler-frontend/src/app/components/game/wait-for-players/player-item/player-item.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { getRandomColor } from '../../../../utility/utility.js';
+
 
 @Component({
-  selector: "app-player-item",
-  templateUrl: "./player-item.component.html",
-  styleUrls: ["./player-item.component.scss"],
+  selector: 'app-player-item',
+  templateUrl: './player-item.component.html',
+  styleUrls: ['./player-item.component.scss'],
 })
 export class PlayerItemComponent implements OnInit {
   @Input() empty: boolean;
   @Input() name: string;
+  @Input() playerId: number;
 
   constructor() {}
 
   ngOnInit() {}
+
+  getRandomColor(inputNumber: number) {
+    return getRandomColor(inputNumber);
+  }
 }
