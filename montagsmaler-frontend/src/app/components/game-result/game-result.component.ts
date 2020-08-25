4c9d78b1698570458818f9b5d08679a85b7d694e
ns107 <ns107@hdm-stuttgart.de>
2020-08-25 20:00:14 +0200

    finally found a carousel

--- montagsmaler-frontend/src/app/components/game-result/game-result.component.ts
+++ montagsmaler-frontend/src/app/components/game-result/game-result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PageSliderModule } from "ng2-page-slider";
+
 
 
 @Component({
@@ -28,7 +30,46 @@ export class GameResultComponent implements OnInit {
         "https://montagsmaler-niklas.s3.amazonaws.com/e713e2b3-4a5f-4306-95b8-9fd2ef1d916b.jpg",
       player: {
         id: "9933fa9d-a011-4a74-829d-b9998b26c58a",
+        name: "Lucas",
+        __className: "Player",
+      },
+      round: 1,
+      score: 0,
+    },
+    {
+      createdAt: 1598356612161,
+      id: "e713e2b3-4a5f-4306-95b8-9fd2ef1d916b",
+      imageUrl:
+        "https://montagsmaler-niklas.s3.amazonaws.com/e713e2b3-4a5f-4306-95b8-9fd2ef1d916b.jpg",
+      player: {
+        id: "9933fa9d-a011-4a74-829d-b9998b26c58a",
+        name: "Jannik",
+        __className: "Player",
+      },
+      round: 1,
+      score: 0,
+    },
+    {
+      createdAt: 1598356612161,
+      id: "e713e2b3-4a5f-4306-95b8-9fd2ef1d916b",
+      imageUrl:
+        "https://montagsmaler-niklas.s3.amazonaws.com/e713e2b3-4a5f-4306-95b8-9fd2ef1d916b.jpg",
+      player: {
+        id: "9933fa9d-a011-4a74-829d-b9998b26c58a",
         name: "niki",
+        __className: "Player",
+      },
+      round: 2,
+      score: 0,
+    },
+    {
+      createdAt: 1598356676981,
+      id: "b537aa0a-c876-49d8-a53b-380d66e73d82",
+      imageUrl:
+        "https://montagsmaler-niklas.s3.amazonaws.com/b537aa0a-c876-49d8-a53b-380d66e73d82.jpg",
+      player: {
+        id: "9933fa9d-a011-4a74-829d-b9998b26c58a",
+        name: "lolig",
         __className: "Player",
       },
       round: 2,
@@ -50,13 +91,42 @@ export class GameResultComponent implements OnInit {
   ];
   rounds = 0;
   imagesPerRound = [];
-
+  slideConfig = { slidesToShow: 1, slidesToScroll: 1 };
 
   constructor() {}
 
   ngOnInit() {
     this.rounds = this.getMaxRound();
-    // this.imagesPerRound = this.getImagesPerRound();
+
+    for (let i = 1; (i <= this.rounds); i++) {
+      this.imagesPerRound[i] = [];
+      this.images.forEach((image) => {
+        if (image.round === i) {
+          this.imagesPerRound[i].push(image);
+        }
+      });
+    }
+    console.log(this.imagesPerRound);
+  }
+
+  addSlide() {}
+
+  removeSlide() {}
+
+  slickInit(e) {
+    console.log("slick initialized");
+  }
+
+  breakpoint(e) {
+    console.log("breakpoint");
+  }
+
+  afterChange(e) {
+    console.log("afterChange");
+  }
+
+  beforeChange(e) {
+    console.log("beforeChange");
   }
 
   getMaxRound() {
--- montagsmaler-frontend/src/app/components/game-result/game-result.module.ts
+++ montagsmaler-frontend/src/app/components/game-result/game-result.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { GameResultComponent } from './game-result.component';
 import { RouterModule } from '@angular/router';
 import { ResultImageComponent } from './result-image/result-image.component';
-
-
+import { SlickCarouselModule } from "ngx-slick-carousel";
 
 
 @NgModule({
   declarations: [GameResultComponent, ResultImageComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SlickCarouselModule],
 })
 export class GameResultModule {}
